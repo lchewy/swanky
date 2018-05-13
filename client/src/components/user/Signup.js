d6c4de907d732282fe71363777437fb81309eaf7
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2018-05-13 14:19:09 -0400

    add fields to signup component

--- client/src/components/user/Signup.js
+++ client/src/components/user/Signup.js
@@ -1,33 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import Modal from "../Modal";
-import {reduxForm} from "redux-form";
+import { reduxForm, Field } from "redux-form";
+import formFields from "./formFields";
+import _ from "lodash";
+import SignupFields from "./SignupFields";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions";
 
-const Signup = ({closeModal}) => {
-  // const { closeModal } = props;
-  return (
-    <Modal>
-      <button onClick={closeModal}>Close Modal</button>
-      <form>
-        <h1>Sign Up</h1>
-        <label htmlFor="fname">
-          <b>First Name</b>
-        </label>
-        <input name="fname" type="text" placeholder="Enter First Name" />
-        <label htmlFor="lname">
-          <b>Last Name</b>
-        </label>
-        <input name="lname" type="text" placeholder="Enter Last Name" />
-        <label htmlFor="email">Email</label>
-        <input name="email" type="text" placeholder="Enter Email" />
-        <label htmlFor="pw">
-          <b>PassWord</b>
-        </label>
-        <input type="password" placeholder="Enter Password" />
+class Signup extends Component {
+  renderFields() {
+    return _.map(formFields, ({ label, name, type }) => {
+      return (
+        <Field
+          key={name}
+          type={type}
+          label={label}
+          name={name}
+          component={SignupFields}
+        />
+      );
+    });
+  }
+  render() {
+    console.log("PROPSSSSS", this.props);
+    const { closeModal, handleSubmit } = this.props;
+    return (
+      <Modal>
+        <div className="modal">
+          <button className="modal__closeBtn" onClick={closeModal}>
+            &times;
+          </button>
+          <form className="modal__form">{this.renderFields()}
+          <button type="submit">Submit</button>
+          </form>
+        </div>
+      </Modal>
+    );
+  }
+}
 
-        <button type="submit">Submit</button>
-      </form>
-    </Modal>
-  );
+const mstp = ({ form }) => {
+  return { formValues: form.signUpForm  };
 };
 
-export default reduxForm({form:"signUpForm"})(Signup);
+export default connect(mstp, actions)(
+  reduxForm({ form: "signUpForm" })(withRouter(Signup))
+);
+
+// <Modal>
+//   <button onClick={closeModal}>Close Modal</button>
+//   <form>
+//     <h1>Sign Up</h1>
+//     <label htmlFor="fname">
+//       <b>First Name</b>
+//     </label>
+//     <input name="fname" type="text" placeholder="Enter First Name" />
+//     <label htmlFor="lname">
+//       <b>Last Name</b>
+//     </label>
+//     <input name="lname" type="text" placeholder="Enter Last Name" />
+//     <label htmlFor="email">Email</label>
+//     <input name="email" type="text" placeholder="Enter Email" />
+//     <label htmlFor="pw">
+//       <b>PassWord</b>
+//     </label>
+//     <input type="password" placeholder="Enter Password" />
+
+//     <button type="submit">Submit</button>
+//   </form>
+// </Modal>
---
+++ client/src/components/user/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: "First Name", name: "fname" , type:"text"},
+  { label: "Last Name", name: "lname", type:"text" },
+  { label: "Email", name: "email", type:"text" },
+  { label: "Password", name: "password", type:"password" }
+];
