e541755054f665b556afda8199353338fa271d49
Leslie Chiu <chiu.les1@gmail.com>
2018-05-18 18:59:42 -0400

    fixed broken img

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,18 +9,23 @@ class Dashboard extends Component {
 
   renderCards() {
     const { products } = this.props;
-    return products.map(product=>{
-        const {_id, title, price, imgUrl} = product;
-        return(
-            <div className="col-1-of-3" key={_id}>
-                <div className="product">
-                    <img height="300px" width="300px" src={`./img/${imgUrl}`} alt={`product-${_id}`}/>
-                    <h5 className="product__title">{title}</h5>
-                    <div className="product__price">${price}</div>
-                </div>
-            </div>
-        )
-    })
+    return products.map(product => {
+      const { _id, title, price, imgUrl } = product;
+      return (
+        <div className="col-1-of-4" key={_id}>
+          <div className="product">
+            <img
+              height="300px"
+              width="300px"
+              src={`./img/${imgUrl}`}
+              alt={`product-${_id}`}
+            />
+            <h5 className="product__title">{title}</h5>
+            <div className="product__price">${price}</div>
+          </div>
+        </div>
+      );
+    });
   }
 
   render() {
