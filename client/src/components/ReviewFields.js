072bce81f7d39e9e55e1038c012f36e55a6eec84
Leslie Chiu <chiu.les1@gmail.com>
2018-05-21 15:27:17 -0400

    adding radio buttons for stars

---
+++ client/src/components/ReviewFields.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () =>{
+    return(
+        <div>
+            <input type="radio"/> 1
+            <input type="radio"/> 2
+            <input type="radio"/> 3
+            <input type="radio"/> 4
+            <input type="radio"/> 5
+            <input type="text"/>
+        </div>
+    )
+}
--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,24 +1,24 @@
-import React, {Component} from "react";
-import {reduxForm, Field} from "redux-form"; 
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
 import * as actions from "../actions";
+import ReviewFields from "./ReviewFields";
 
-class Reviews extends Component{
-    renderField(){
-        return(
-            <div>
-                <p>HELLO</p>
-            </div>
-        )
-    }
+class Reviews extends Component {
+  renderFields() {
+    return <Field
+        label="Review"
+        type="text"
+        name="review"
+        component={ReviewFields}
+    />;
+  }
 
-    render(){
-        return(
-            <form>
-                {this.renderFields()}
-            </form>
-        )
-    }
+  render() {
+    return <form>{this.renderFields()}</form>;
+  }
 }
-const mstp = ()=>{};
-export default connect(mstp, actions)(reduxForm({form:"reviewForm"}))(Reviews);
+const mstp = ({auth}) => ({auth});
+export default connect(mstp, actions)(
+  reduxForm({ form: "reviewForm" })(Reviews)
+);
