5250fad88a9f5a6872f6c6effb9ee62e8765c5d2
Leslie Chiu <chiu.les1@gmail.com>
2018-05-22 15:33:05 -0400

    rating stars working

--- client/src/components/ReviewFields.js
+++ client/src/components/ReviewFields.js
@@ -1,22 +1,24 @@
 import React from "react";
 
 export default (values) => {
-  const{input, type } = values
+  const{input, type} = values
+  console.log("VAL ",values)
   return (
     <div className="rate" >
-      <input className="rate__radioa" {...input} type={type} id="star5" value="5"/>
-      <label className="rate__label" htmlFor="star5"></label>
+      
+      <input className="rate__radioa" {...input} type={type} id="star5" value={5}/>
+      <label className="rate__label" htmlFor="star5"/>
 
-      <input className="rate__radioa" {...input} type={type} id="star4" value="4"/>
+      <input className="rate__radioa" {...input} type={type} id="star4" value={4}/>
       <label className="rate__label" htmlFor="star4"></label>
 
-      <input className="rate__radioa" {...input} type={type} id="star3" value="3"/>
+      <input className="rate__radioa" {...input} type={type} id="star3" value={3}/>
       <label className="rate__label" htmlFor="star3"></label>
 
-      <input className="rate__radioa" {...input} type={type} id="star2" value="2"/>
+      <input className="rate__radioa" {...input} type={type} id="star2" value={2}/>
       <label className="rate__label" htmlFor="star2"></label>
 
-      <input className="rate__radioa" {...input} type={type} id="star1" value="1"/>
+      <input className="rate__radioa" {...input} type={type} id="star1" value={1}/>
       <label className="rate__label" htmlFor="star1"></label>
     </div>
   );
--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -5,19 +5,29 @@ import * as actions from "../actions";
 import ReviewFields from "./ReviewFields";
 
 class Reviews extends Component {
-
   renderFields() {
     const { handleSubmit, auth } = this.props;
     if (auth) {
       return (
         <form onSubmit={handleSubmit(() => this.submitReview())}>
-          <Field type="radio" component={ReviewFields} name="rating"/>
-          <Field type="text" component="textarea" name="summary"/>
-          <button type="submit">Submit</button>
+          <div className="rate">
+            <Field className="rate__radio" id="rating5" name="rating" component="input" type="radio" value="5"/>
+            <label className="rate__label" htmlFor="rating5" />
+            <Field className="rate__radio" id="rating4" name="rating" component="input" type="radio" value="4" />
+            <label className="rate__label" htmlFor="rating4" />
+            <Field className="rate__radio" id="rating3" name="rating" component="input" type="radio" value="3" />
+            <label className="rate__label" htmlFor="rating3" />
+            <Field className="rate__radio" id="rating2" name="rating" component="input" type="radio" value="2" />
+            <label className="rate__label" htmlFor="rating2" />
+            <Field className="rate__radio" id="rating1" name="rating" component="input" type="radio" value="1" />
+            <label className="rate__label" htmlFor="rating1" />
+            <Field type="text" component="textarea" name="summary" />
+            <button type="submit">Submit</button>
+          </div>
         </form>
       );
     } else {
-      return <div>Please sign up or log in to leave review</div>
+      return <div>Please sign up or log in to leave review</div>;
     }
   }
 
@@ -39,3 +49,5 @@ const mstp = ({ auth, product, form }) => ({
 export default connect(mstp, actions)(
   reduxForm({ form: "reviewForm" })(Reviews)
 );
+
+// <Field type="radio" component={ReviewFields} name="rating" value="me"/>
