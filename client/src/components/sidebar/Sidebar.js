76fc27efc951619e383975d227f73d73f840b4b5
Leslie Chiu <chiu.les1@gmail.com>
2018-05-30 21:19:57 -0400

    sidebar css

--- client/src/components/sidebar/Sidebar.js
+++ client/src/components/sidebar/Sidebar.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Sidebar = ()=>{
     return(
-        <div style={{textAlign:"center", fontSize:"40px"}} className="sidebar">SIDEBAR</div>
+        <div style={{textAlign:"center", fontSize:"40px"}} className="sidebar">
+        <p className="sidebar__text">SIDEBAR</p>
+        </div>
     )
 }
 
